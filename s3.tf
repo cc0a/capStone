387b4465edbe4260235588f04bb6dfadb0d20642
John Smith <jmckeeco84@gmail.com>
2026-01-19 14:48:20 -0700

    ECS Repair

--- s3.tf
+++ s3.tf
@@ -1,24 +1,24 @@
-# resource "aws_s3_bucket" "vpc_logs" {
-#   bucket = "my-vpc-flow-logs-bucket-12345"
-# }
+resource "aws_s3_bucket" "vpc_logs" {
+  bucket = "my-vpc-flow-logs-bucket-12345"
+}
 
-# resource "aws_s3_bucket_lifecycle_configuration" "vpc_logs" {
-#   bucket = aws_s3_bucket.vpc_logs.id
+resource "aws_s3_bucket_lifecycle_configuration" "vpc_logs" {
+  bucket = aws_s3_bucket.vpc_logs.id
 
-#   rule {
-#     id     = "logs"
-#     status = "Enabled"
+  rule {
+    id     = "logs"
+    status = "Enabled"
 
-# transition {
-#   days          = 30
-#   storage_class = "STANDARD_IA"
-# }
+transition {
+  days          = 30
+  storage_class = "STANDARD_IA"
+}
 
-# transition {
-#   days          = 90
-#   storage_class = "DEEP_ARCHIVE"
-# }
+transition {
+  days          = 90
+  storage_class = "DEEP_ARCHIVE"
+}
 
-#     expiration { days = 365 }
-#   }
-# }
+    expiration { days = 365 }
+  }
+}
