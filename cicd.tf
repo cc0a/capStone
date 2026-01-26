da138d3c903187d8145d32e403c27a8b2e3c40b0
John Smith <jmckeeco84@gmail.com>
2026-01-25 19:00:39 -0700

    remove notes.txt

--- cicd.tf
+++ cicd.tf
@@ -1,26 +1,41 @@
 ###########################################
-# IAM Role for CodeBuild
+# RANDOM SUFFIX FOR UNIQUE BUCKET NAME
+###########################################
+resource "random_id" "suffix" {
+  byte_length = 4
+}
+
+###########################################
+# S3 BUCKET FOR PIPELINE ARTIFACTS
+###########################################
+resource "aws_s3_bucket" "codepipeline_artifacts" {
+  bucket = "wordpress-codepipeline-artifacts-${random_id.suffix.hex}"
+}
+
+resource "aws_s3_bucket_versioning" "codepipeline_artifacts_versioning" {
+  bucket = aws_s3_bucket.codepipeline_artifacts.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+###########################################
+# IAM ROLE FOR CODEBUILD
 ###########################################
 resource "aws_iam_role" "codebuild_role" {
   name = "wordpress-codebuild-role"
 
   assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect    = "Allow"
-        Principal = {
-          Service = "codebuild.amazonaws.com"
-        }
-        Action = "sts:AssumeRole"
-      }
-    ]
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "codebuild.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
   })
 }
 
-###########################################
-# IAM Policy Attachments
-###########################################
 resource "aws_iam_role_policy_attachment" "codebuild_ecr_access" {
   role       = aws_iam_role.codebuild_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryPowerUser"
@@ -32,7 +47,7 @@ resource "aws_iam_role_policy_attachment" "codebuild_logs_access" {
 }
 
 ###########################################
-# CodeBuild Project
+# CODEBUILD PROJECT
 ###########################################
 resource "aws_codebuild_project" "wordpress" {
   name         = "wordpress-build"
@@ -62,5 +77,113 @@ resource "aws_codebuild_project" "wordpress" {
   source {
     type      = "CODEPIPELINE"
     buildspec = "buildspec.yml"
+  }
+}
+
+###########################################
+# IAM ROLE FOR CODEPIPELINE
+###########################################
+resource "aws_iam_role" "codepipeline_role" {
+  name = "wordpress-codepipeline-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Effect    = "Allow",
+      Principal = { Service = "codepipeline.amazonaws.com" },
+      Action    = "sts:AssumeRole"
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "pipeline_attach_s3" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3FullAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "pipeline_attach_codebuild" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSCodeBuildDeveloperAccess"
+}
+
+resource "aws_iam_role_policy_attachment" "pipeline_attach_ecs" {
+  role       = aws_iam_role.codepipeline_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonECS_FullAccess"
+}
+
+###########################################
+# CODESTAR CONNECTION FOR GITHUB
+###########################################
+resource "aws_codestarconnections_connection" "github" {
+  name          = "github-connection"
+  provider_type = "GitHub"
+}
+
+###########################################
+# CODEPIPELINE CONFIGURATION
+###########################################
+resource "aws_codepipeline" "wordpress_pipeline" {
+  name     = "wordpress-pipeline"
+  role_arn = aws_iam_role.codepipeline_role.arn
+
+  artifact_store {
+    type     = "S3"
+    location = aws_s3_bucket.codepipeline_artifacts.bucket
+  }
+
+  stage {
+    name = "Source"
+
+    action {
+      name             = "GitHub_Source"
+      category         = "Source"
+      owner            = "AWS"
+      provider         = "CodeStarSourceConnection"
+      version          = "1"
+      output_artifacts = ["source_output"]
+
+      configuration = {
+        ConnectionArn    = aws_codestarconnections_connection.github.arn
+        FullRepositoryId = "cc0a/wp-ecs"
+        BranchName       = "main"
+      }
+    }
+  }
+
+  stage {
+    name = "Build"
+
+    action {
+      name             = "Docker_Build"
+      category         = "Build"
+      owner            = "AWS"
+      provider         = "CodeBuild"
+      version          = "1"
+      input_artifacts  = ["source_output"]
+      output_artifacts = ["build_output"]
+
+      configuration = {
+        ProjectName = aws_codebuild_project.wordpress.name
+      }
+    }
+  }
+
+  stage {
+    name = "Deploy"
+
+    action {
+      name            = "ECS_Deploy"
+      category        = "Deploy"
+      owner           = "AWS"
+      provider        = "ECS"
+      version         = "1"
+      input_artifacts = ["build_output"]
+
+      configuration = {
+        ClusterName = "wordpress-cluster"
+        ServiceName = "wordpress-service"
+        FileName    = "imagedefinitions.json"
+      }
+    }
   }
 }
